ca8bcceebedb46a453f5dd5b4abb4f72b742f04b
jpbrasile <brasile@effitech.eu>
2021-01-20 08:37:33 +0100

    test commit

---
+++ MyProject/src/MyProject.jl
@@ -0,0 +1,14 @@
+module MyProject
+import Pkg
+Pkg.add("ForwardDiff")
+using ForwardDiff
+f(x)=2x+3
+#greet() = print("Hello World!")
+ForwardDiff.derivative(f,2)
+#creation d'un package local (dans .julia/dev)
+#Pkg.add("PkgTemplates")
+#using PkgTemplates
+#t=Template(user="jpbrasile")
+#t("demo2")
+Pkg.add("demo2")
+end # module
